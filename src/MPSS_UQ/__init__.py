18729923695b5db669a7a1cf2b04d89cd1782bc2
Matti Niskanen <matti.niskanen@uef.fi>
2026-02-07 22:29:26 +0200

    Set environmental variables at the package's entry point

--- src/MPSS_UQ/__init__.py
+++ src/MPSS_UQ/__init__.py
@@ -1,1 +1,8 @@
 # -*- coding: utf-8 -*-
+
+import os
+
+os.environ.setdefault("OMP_NUM_THREADS", "1")
+os.environ.setdefault("MKL_NUM_THREADS", "1")
+os.environ.setdefault("OPENBLAS_NUM_THREADS", "1")
+os.environ.setdefault("NUMEXPR_NUM_THREADS", "1")
